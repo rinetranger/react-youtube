a764b0900ff51264f85303c82008ea09bcc092ca
Rin Kishi <rinetranger@gmail.com>
2020-10-28 16:25:56 +0900

    MakingVideoGrid

---
+++ src/components/VideoGrid/VideoGrid.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Style from './VideoGrid.module.scss'
+
+const VideoGrid = ({children}) => {
+    return (
+        <div className={Style.container}>
+            
+        </div>
+    )
+}
+
+export default VideoGrid
