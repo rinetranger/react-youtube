ddc1946e8c7e89e4be9ea6be4b7188ac60ba3aad
Rin Kishi <rinetranger@gmail.com>
2020-10-28 14:09:44 +0900

    headerCSS

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,12 +2,16 @@ import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faSearch } from '@fortawesome/free-solid-svg-icons'
 import {Link} from 'react-router-dom'
+import Style from './Header.module.scss'
 
 const Header = () => {
     return (
-        <div>
-            <Link to="/">VideoTube</Link>
-            <div>
+        <div className={Style.header}>
+            <div className={Style.item}>
+                <Link to="/">VideoTube</Link>
+            </div>
+            
+            <div className={Style.item}>
                 <form>
                     <input type="text" placeholder="search" />
                     <button type="submit">
