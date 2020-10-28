ee794600027e03a25a499b36ec749710e2de2380
Rin Kishi <rinetranger@gmail.com>
2020-10-28 16:17:18 +0900

    layoutCSS&normailze

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import Header from '../Header/Header'
+import Style from './Layout.module.scss'
 
 const Layout = ({children}) => {
     return (
-        <div>
+        <div className={Style.wrapper}>
             <Header />
-            {children}
+            <div className={Style.main}>
+                {children}
+            </div>
+            
         </div>
     )
 }
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { StoreProvider } from './store/index';  
 import App from './App';
+import 'normalize.css'
 import reportWebVitals from './reportWebVitals';
 
 
