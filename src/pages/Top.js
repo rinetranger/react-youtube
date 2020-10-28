9c8de75fb1ce3d8279d92be1de245a73d15856bb
Rin Kishi <rinetranger@gmail.com>
2020-10-28 13:33:59 +0900

    Making-Layout&Headerstructure

--- src/pages/Top.js
+++ src/pages/Top.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import Layout from '../components/Layout/Layout'
 
 const Top = () => {
     return (
         <div>
+            <Layout>
             Toppage
+            </Layout>
         </div>
     )
 }
--- src/pages/Watch.js
+++ src/pages/Watch.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import Layout from '../components/Layout/Layout'
 
 const Watch = () => {
     return (
         <div>
-           watchpage
+            <Layout>
+                Watchpage
+            </Layout>
         </div>
     )
 }
