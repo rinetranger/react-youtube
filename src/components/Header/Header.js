a2004394308c830ff1254e52b8b2068fc7bac296
Rin Kishi <rinetranger@gmail.com>
2020-10-28 13:54:22 +0900

    sass,fontawsome,headermaking

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSearch } from '@fortawesome/free-solid-svg-icons'
+import {Link} from 'react-router-dom'
 
 const Header = () => {
     return (
         <div>
-            Header
+            <Link to="/">VideoTube</Link>
+            <div>
+                <form>
+                    <input type="text" placeholder="search" />
+                    <button type="submit">
+                        <FontAwesomeIcon icon={faSearch} />
+                    </button>
+                </form>
+            </div>
         </div>
     )
 }
