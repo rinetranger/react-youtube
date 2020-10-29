620bb48fc7bd6ee1ca1dca10fd146bce1ec8ac91
Rin Kishi <rinetranger@gmail.com>
2020-10-29 11:21:21 +0900

    playerresponsible

--- src/components/VideoPlay/VideoPlay.js
+++ src/components/VideoPlay/VideoPlay.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import Youtube from 'react-youtube'
-
+import Style from './VideoPlay.module.scss'
 
 const VideoPlay = ({id}) => {
     return (
-        <div>
-            <Youtube videoId={id} />
+        <div className={Style.wrap}>
+            <Youtube className={Style.video} videoId={id} />
         </div>
     )
 }
