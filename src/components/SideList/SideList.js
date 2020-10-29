aba9c003ad874cd41cd8f3b3a11eb51ce529a57b
Rin Kishi <rinetranger@gmail.com>
2020-10-30 00:02:57 +0900

    renderingSideList

--- src/components/SideList/SideList.js
+++ src/components/SideList/SideList.js
@@ -1,6 +1,8 @@
 import React ,{useEffect,useContext}from 'react'
 import {Store} from '../../store/index'
 import {fetchRelatedData} from '../../apis/index'
+import SideListItem from '../SideListItem/SideListItem'
+import Style from './SideList.module.scss'
 
 const SideList = () => {
     const {globalState,setGlobalState} =useContext(Store)
@@ -13,8 +15,19 @@ const SideList = () => {
         setRelatedVideo(globalState.selected.id)
     },[globalState.selected])
     return (
-        <div>
-            
+        <div className={Style.sidenav}>
+            {
+                globalState.related ?globalState.related.map((video)=>{
+                    return(
+                        <SideListItem
+                        id={video.id.videoId}
+                        key={video.id.videoId}
+                        src={video.snippet.thumbnails.medium.url}
+                        title={video.snippet.title} 
+                        />
+                    )
+                }): <span>no data</span>
+            }
         </div>
     )
 }
