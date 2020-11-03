8be1fb4dd19d39bd512305e55572e36931eb1cdc
Rin Kishi <rinetranger@gmail.com>
2020-11-03 18:22:50 +0900

    searchtermsetglobalstate

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,22 @@
-import React from 'react'
+import React,{useState,useEffect,useContext} from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faSearch } from '@fortawesome/free-solid-svg-icons'
-import {Link} from 'react-router-dom'
+import {Link, useHistory} from 'react-router-dom'
 import Style from './Header.module.scss'
+import {Store} from '../../store/index'
 
 const Header = () => {
+    const [term,setTerm]=useState('')
+    const history =useHistory()
+    const {globalState,setGlobalState}=useContext(Store)
+    const handleSubmit= e =>{
+        e.preventDefault()
+        setGlobalState({type:'SET_TERM',payload:{term}})
+        history.push(`/search?query=${term}`)
+    }
+    useEffect(()=>{
+        setTerm(globalState.term)
+    },[])
     return (
         <div className={Style.header}>
             <div className={Style.item}>
@@ -12,8 +24,12 @@ const Header = () => {
             </div>
             
             <div className={Style.item}>
-                <form>
-                    <input type="text" placeholder="search" />
+                <form onSubmit={handleSubmit}>
+                    <input 
+                        type="text" 
+                        placeholder="search" 
+                        onChange={ e =>setTerm(e.target.value)} 
+                        value={term}/>
                     <button type="submit">
                         <FontAwesomeIcon icon={faSearch} />
                     </button>
--- src/store/index.js
+++ src/store/index.js
@@ -3,7 +3,8 @@ import React,{createContext, useReducer} from 'react'
 const initialState= {
     popular:[],
     related:[],
-    selected:{}
+    selected:{},
+    term:''
 }
 const reducer = (state,action)=>{
     switch(action.type){
@@ -13,6 +14,8 @@ const reducer = (state,action)=>{
             return {...state,related:action.payload.related}
         case 'SET_SELECTED':
             return {...state,selected:action.payload.selected}
+        case 'SET_TERM':
+            return {...state,term:action.payload.term}
         default:
             return state
         }
